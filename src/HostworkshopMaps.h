cb210df713b6748b5bb6274d8849c8ab70dcac6a
Kareem-Kareem-kareem <kareemabidin1@gmail.com>
2026-06-25 16:53:49 +0300

    Update HostworkshopMaps.h

--- src/HostworkshopMaps.h
+++ src/HostworkshopMaps.h
@@ -1,25 +1,27 @@
 #pragma once
 
+#include <filesystem>
 #include <vector>
 #include <string>
-#include <filesystem>
 #include "bakkesmod/plugin/bakkesmodplugin.h"
 #include "bakkesmod/plugin/pluginwindow.h"
 
-class HostWorkshopMaps : public BakkesMod::Plugin::BakkesModPlugin, public BakkesMod::Plugin::PluginWindow
+class HostWorkshopMaps : public BakkesMod::Plugin::BakkesModPlugin
+                       , public BakkesMod::Plugin::PluginWindow
 {
 public:
     virtual void onLoad() override;
     virtual void onUnload() override;
-    
-    virtual void Render() override;
+
+    // -- PluginWindow pure virtuals (ALL REQUIRED) --
+    virtual void        Render() override;
     virtual std::string GetMenuName() override;
     virtual std::string GetMenuTitle() override;
-    virtual void SetImGuiContext(uintptr_t ctx) override;
-    virtual bool ShouldBlockInput() override;
-    virtual bool IsActiveOverlay() override;
-    virtual void OnOpen() override;
-    virtual void OnClose() override;
+    virtual void        SetImGuiContext(uintptr_t ctx) override;
+    virtual bool        ShouldBlockInput() override;
+    virtual bool        IsActiveOverlay() override;
+    virtual void        OnOpen() override;
+    virtual void        OnClose() override;
 
 private:
     void scanMapsDirectory();
@@ -27,7 +29,7 @@ private:
     void loadMapByIndex(int index);
     void loadMapByPath(const std::string& path);
     bool isLanHost();
-    
+
     std::vector<std::string> mapFiles;
     std::vector<std::string> mapNames;
 };
