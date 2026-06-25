3fc7dae28db9237f0743884e08ad604e5bcb156d
Kareem-Kareem-kareem <kareemabidin1@gmail.com>
2026-06-25 16:08:30 +0300

    Update HostworkshopMaps.h

--- src/HostworkshopMaps.h
+++ src/HostworkshopMaps.h
@@ -1,26 +1,23 @@
 #pragma once
 
-#include <string>
-#include <vector>
 #include <filesystem>
-
+#include <vector>
+#include <string>
 #include "bakkesmod/plugin/bakkesmodplugin.h"
 #include "bakkesmod/plugin/pluginwindow.h"
 
-class HostWorkshopMaps : public BakkesMod::Plugin::BakkesModPlugin,
-                         public BakkesMod::Plugin::PluginWindow
+class HostWorkshopMaps : public BakkesMod::Plugin::BakkesModPlugin, public BakkesMod::Plugin::PluginWindow
 {
 public:
     // BakkesModPlugin overrides
     virtual void onLoad() override;
     virtual void onUnload() override;
 
-    // PluginWindow override - only called if RegisterDrawable is enabled
+    // PluginWindow overrides - THESE ARE ALL REQUIRED
     virtual void Render() override;
-
-    // Plugin metadata
     virtual std::string GetMenuName() override;
     virtual std::string GetMenuTitle() override;
+    virtual void SetImGuiContext(uintptr_t ctx) override;  // THIS WAS MISSING!
     virtual bool ShouldBlockInput() override;
     virtual bool IsActiveOverlay() override;
     virtual void OnOpen() override;
