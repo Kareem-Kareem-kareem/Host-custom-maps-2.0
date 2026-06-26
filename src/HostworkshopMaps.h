35e057ffad78ac6ef69f5b554f37d427108ef0a6
Kareem-Kareem-kareem <kareemabidin1@gmail.com>
2026-06-26 17:06:34 +0300

    Update HostworkshopMaps.h

--- src/HostworkshopMaps.h
+++ src/HostworkshopMaps.h
@@ -1,16 +1,15 @@
 #pragma once
 #include "bakkesmod/plugin/bakkesmodplugin.h"
-#include <string>
 #include <vector>
-#include <Windows.h>
+#include <string>
+#include <algorithm>
 
 struct MapEntry {
     std::string displayName;
     std::string fullPath;
 };
 
-class HostWorkshopMaps : public BakkesMod::Plugin::BakkesModPlugin
-{
+class HostWorkshopMaps : public BakkesMod::Plugin::BakkesModPlugin {
 public:
     void onLoad() override;
     void onUnload() override;
@@ -18,9 +17,7 @@ public:
 private:
     void ScanMaps();
     void LoadMap(int index, bool isLAN);
-
     void SetStatus(const std::string& msg);
-
     static std::string MapNameFromPath(const std::string& path);
 
     std::vector<MapEntry> mapList_;
