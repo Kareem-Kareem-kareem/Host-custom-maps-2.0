19af98fe1971f76edb9eba64ddaafd7f44d1e58c
Kareem-Kareem-kareem <kareemabidin1@gmail.com>
2026-06-26 16:34:47 +0300

    Update HostworkshopMaps.h

--- src/HostworkshopMaps.h
+++ src/HostworkshopMaps.h
@@ -1,6 +1,5 @@
 #pragma once
 #include "bakkesmod/plugin/bakkesmodplugin.h"
-#include "imgui/imgui.h"
 #include <string>
 #include <vector>
 #include <Windows.h>
@@ -15,19 +14,16 @@ class HostWorkshopMaps : public BakkesMod::Plugin::BakkesModPlugin
 public:
     void onLoad() override;
     void onUnload() override;
-    void Render(CanvasWrapper canvas);
 
 private:
     void ScanMaps();
-    void LoadMap(const std::string& path, bool isLAN);
+    void LoadMap(int index, bool isLAN);
 
     void SetStatus(const std::string& msg);
-    std::string AutoDetectMapsPath();
 
     static std::string MapNameFromPath(const std::string& path);
 
     std::vector<MapEntry> mapList_;
-    std::string subFolder_ = "Mods";
+    std::string subFolder_ = "mods";   // default lowercase
     std::string statusMsg_;
-    bool showWindow_ = true;
 };
