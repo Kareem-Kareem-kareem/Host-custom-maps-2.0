012a3d2da046d7226a36116f7cb318fb64f9cd0a
Kareem-Kareem-kareem <kareemabidin1@gmail.com>
2026-06-27 11:11:57 +0300

    Update HostworkshopMaps.h

--- src/HostworkshopMaps.h
+++ src/HostworkshopMaps.h
@@ -1,32 +1,33 @@
 #pragma once
 #include "bakkesmod/plugin/bakkesmodplugin.h"
+#include "bakkesmod/plugin/PluginSettingsWindow.h"
 #include <vector>
 #include <string>
 #include <algorithm>
 
-struct MapEntry
-{
+struct MapEntry {
     std::string displayName;
     std::string fullPath;
 };
 
-class HostWorkshopMaps : public BakkesMod::Plugin::BakkesModPlugin
+class HostWorkshopMaps : public BakkesMod::Plugin::BakkesModPlugin, public BakkesMod::Plugin::PluginSettingsWindow
 {
 public:
     void onLoad() override;
     void onUnload() override;
+
+    // PluginSettingsWindow implementation (F2 -> Plugins -> Host Workshop Maps)
+    void RenderSettings() override;
+    std::string GetPluginName() override;
+    void SetImGuiContext(uintptr_t ctx) override;
 
 private:
     void ScanMaps();
     void LoadMap(int index, bool isLAN);
     void SetStatus(const std::string& msg);
     static std::string MapNameFromPath(const std::string& path);
-    void RenderUI();
 
     std::vector<MapEntry> mapList_;
     std::string mapsDirectory_;
     std::string statusMsg_;
-    bool uiOpen_ = false;
-    int selectedMapIndex_ = -1;
-    char directoryBuffer_[512] = "";
 };
